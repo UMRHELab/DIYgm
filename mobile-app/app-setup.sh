364d502f5820e24825997aaa69de06163c0af8a1
Santa-Bunny <AJEKAKENT@gmail.com>
2021-05-17 17:41:03 -0400

    Fixed echo command

--- mobile-app/app-setup.sh
+++ mobile-app/app-setup.sh
@@ -1,8 +1,8 @@
-echo("---------------- Update ----------------")
+echo "---------------- Update ----------------\n"
 # Make sure the system is up to date
 sudo apt-get update
 
-echo("---------------- Installing Dependencies ----------------")
+echo "---------------- Installing Dependencies ----------------\n"
 # Install all dependencies
 sudo apt-get install python-bluez  
 sudo pip3 install pybluez  
@@ -11,34 +11,34 @@ sudo pip3 install pybluez
 #install bluetooth-dev
 sudo apt-get install -y libbluetooth-dev
 
-echo("---------------- Modifying bluetooth for compatibility ----------------")
+echo "---------------- Modifying bluetooth for compatibility ----------------\n"
 #add --compat to /etc/systemd/system/dbus-org.bluez.service
 sudo sed -i 's_ExecStart=/usr/lib/bluetooth/bluetoothd_ExecStart=/usr/lib/bluetooth/bluetoothd --compat_g' /etc/systemd/system/dbus-org.bluez.service
 
-echo("---------------- Restarting Bluetooth ----------------")
+echo "---------------- Restarting Bluetooth ----------------\n"
 #restart all bluetooth fucntionality
 sudo systemctl daemon-reload
 sudo systemctl restart bluetooth
 sudo chmod 755 /var/run/sdp
 
-echo("---------------- Copying Necessary Files to root ----------------")
+echo "---------------- Copying Necessary Files to root ----------------\n"
 #move all necessary files to root
 sudo cp -R /home/pi/DIYgm/mobile-app/* /
 
-echo("---------------- Extracting Node.js ----------------")
+echo "---------------- Extracting Node.js ----------------\n"
 #setup arm bluetooth node extension
 sudo tar -xf /node-v4.3.1-linux-armv6l.tar.gz
 sudo service bluetooth stop
 sudo cp -R /node-v4.3.1-linux-armv6l/* /usr/local
 sudo service bluetooth start
 
-echo("---------------- Adding Executable Status to Program ----------------")
+echo "---------------- Adding Executable Status to Program ----------------\n"
 #mark all executables as executable
 sudo chmod +x /startup.sh
 sudo chmod +x /discoverable.sh
 sudo chmod +x /auto-launch.sh
 
-echo("---------------- Adding Cronjob ----------------")
+echo "---------------- Adding Cronjob ----------------\n"
 sudo crontab -l | sudo crontab -
 
 #set up crontab V2.0
